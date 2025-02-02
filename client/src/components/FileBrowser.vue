a58172eab0f946e7920f214b18f75e7e8afd436c
Destin Moulton <destin.moulton@gmail.com>
2025-02-02 09:49:43 -0700

    Added basic file type icons.

--- client/src/components/FileBrowser.vue
+++ client/src/components/FileBrowser.vue
@@ -1,12 +1,19 @@
+
 <template>
   <ul class="list-group">
-    <li class="list-group-item" v-for="file in files" :key="file.name">{{ file.name }}</li>
+    <li class="list-group-item" v-for="file in files" :key="file.name">
+      <span v-if="file.is_directory"><Directory :name="file.name"></Directory></span>
+      <span v-else><File :name="file.name"></File></span>
+    </li>
   </ul>
 </template>
 <script>
 import { mapState  } from 'vuex'
+import Directory from '@/components/FileBrowser/FiletypeDirectory.vue'
+import File from '@/components/FileBrowser/FiletypeFile.vue'
 
 export default {
+  components: { Directory, File},
   computed: mapState({
     files: (state) => state.files.all,
   }),
---
+++ client/src/components/FileBrowser/FiletypeDirectory.vue
@@ -0,0 +1,14 @@
+<template>
+  <FontAwesomeIcon :icon="faFolder"/> {{name}}
+</template>
+<script setup>
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import {faFolder} from '@fortawesome/free-regular-svg-icons';
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  }
+});
+</script>
