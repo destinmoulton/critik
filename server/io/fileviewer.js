cb761816abd4453039c9d1aa12d5fe6767c8e71f
Destin Moulton <destin.moulton@gmail.com>
2025-02-04 10:17:24 -0700

    Added markdown to html converter (showdown).

--- server/io/fileviewer.js
+++ server/io/fileviewer.js
@@ -1,6 +1,7 @@
 const fsp = require('fs').promises;
 const os = require('os');
 const path = require('path');
+const showdown = require('showdown');
 module.exports = (io, socket) => {
     socket.on('server:fileviewer:getfile', async (params) => {
         console.log('socket.io : server:fileviewer:getfile', params);
@@ -11,10 +12,11 @@ module.exports = (io, socket) => {
         let file_html = '';
         let status = 'success';
         try {
+            let converter = new showdown.Converter();
             const file_contents = await fsp.readFile(path_to_file, 'utf-8');
             console.log('socket.io : file_contents', file_contents);
             file_original_contents = file_contents;
-            file_html = file_contents;
+            file_html = converter.makeHtml(file_contents);
         } catch (err) {
             status = 'error';
             error = err.message;
